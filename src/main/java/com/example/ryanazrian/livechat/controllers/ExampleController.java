4e69fce308f266e0200d2f17015eb3650b1ad88d
Ryan Azrian Syach <ryanazrian@pahamify.com>
2020-03-02 17:28:13 +0700

    Adding Post Method

--- src/main/java/com/example/ryanazrian/livechat/controllers/ExampleController.java
+++ src/main/java/com/example/ryanazrian/livechat/controllers/ExampleController.java
@@ -2,9 +2,7 @@ package com.example.ryanazrian.livechat.controllers;
 
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/example")
@@ -14,4 +12,12 @@ public class ExampleController {
     public ResponseEntity<String> get(){
         return ResponseEntity.ok("Hello Yan!!");
     }
+
+    @PostMapping(path = "/members", consumes = "application/json", produces = "application/json")
+    public ResponseEntity<String> addMember(@RequestBody String ast){
+        System.out.println(ast);
+
+        return ResponseEntity.ok("MANTAP");
+    }
+
 }
