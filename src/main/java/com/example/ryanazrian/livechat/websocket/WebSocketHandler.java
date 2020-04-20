f94f5b81a7079bb8b95f88e03f019ee04ca2fcb5
ryanazrian <ryanazrian@pahamify.com>
2020-04-20 23:14:41 +0700

    handling message

--- src/main/java/com/example/ryanazrian/livechat/websocket/WebSocketHandler.java
+++ src/main/java/com/example/ryanazrian/livechat/websocket/WebSocketHandler.java
@@ -11,26 +11,28 @@ public class WebSocketHandler extends AbstractWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws IOException {
         String msg = String.valueOf(message.getPayload());
         // Send back unique message depending on the id received from the client
-        switch(msg){
-            case("1"):
-                System.out.println("Dog button was pressed");
-                session.sendMessage(new TextMessage("Woooof"));
-                break;
-            case("2"):
-                System.out.println("Cat button was pressed");
-                session.sendMessage(new TextMessage("Meooow"));
-                break;
-            case("3"):
-                System.out.println("Pig button was pressed");
-                session.sendMessage(new TextMessage("Bork Bork"));
-                break;
-            case("4"):
-                System.out.println("Fox button was pressed");
-                session.sendMessage(new TextMessage("Fraka-kaka-kaka"));
-                break;
-            default:
-                System.out.println("Connected to Client");
-        }
+        System.out.println(msg);
+        session.sendMessage(new TextMessage("MANATP JIWA GUYS"));
+//        switch(msg){
+//            case("1"):
+//                System.out.println("Dog button was pressed");
+//                session.sendMessage(new TextMessage("Woooof"));
+//                break;
+//            case("2"):
+//                System.out.println("Cat button was pressed");
+//                session.sendMessage(new TextMessage("Meooow"));
+//                break;
+//            case("3"):
+//                System.out.println("Pig button was pressed");
+//                session.sendMessage(new TextMessage("Bork Bork"));
+//                break;
+//            case("4"):
+//                System.out.println("Fox button was pressed");
+//                session.sendMessage(new TextMessage("Fraka-kaka-kaka"));
+//                break;
+//            default:
+//                System.out.println("Connected to Client");
+//        }
     }
 
 }
